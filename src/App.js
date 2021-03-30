4330e2045b1a644f749f65136ef922189e322bd4
Devdatt17 <devdattpandit99@gmail.com>
2021-03-31 00:09:53 +0530

    Add and Delete Notes feature added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+
+import React from 'react'
+import uuid from 'react-uuid'
 import './App.css';
 import Sidebar from './components/Sidebar'
 import Main from './components/Main'
 
 function App() {
+  
+  const [notes,setNotes] = React.useState([]);
+
+  const onAddNotes = ()=>{
+
+      const newNote = {
+        id: uuid(),
+        title: "Untitled Note",
+        body:"",
+        lastModified:Date.now(),
+      }
+
+      setNotes([newNote, ...notes])
+  }
+
+  const onDeleteNote = (idToDelete)=>{
+    //? notes are filtered such that only the notes with id same as that of those passed form 
+    //? delete event in sidebar are only rendered.
+      setNotes(notes.filter((note)=>note.id !== idToDelete));
+  }
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar 
+      notes={notes}
+      onAddNotes={onAddNotes}
+      onDeleteNote={onDeleteNote}
+      />
       <Main />
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,33 @@
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = (props) => {
     return (
         <div className="app-sidebar">
             <div className="app-sidebar-header">
                 <h1>Dev's Notes</h1>
-                <button>Add</button>
+                <button onClick={props.onAddNotes}>Add</button>
             </div>
             <div className="app-sidebar-notes">
-                <div className="app-sidebar-note">
-                    <div className="sidebar-note-title">
-                        <strong>TITLE</strong>
-                        <button>Delete</button>
-                    </div>
-                    <p>Notes Preview</p>
-                    <small className="note-meta">
-                        Last modified [date]
-                    </small>
-                </div>
+                {
+                    props.notes.map((items) => (
+                        <div className="app-sidebar-note">
+                            <div className="sidebar-note-title">
+                                <strong>{ items.title }</strong>
+                                <button onClick={()=>props.onDeleteNote(items.id)}>Delete</button>
+                            </div>
+                            
+                            <p>{ items.body && items.body.substr(0,100)+"..." }</p>
+                            
+                            <small className="note-meta">
+                                Last modified { new Date(items.lastModified).toLocaleDateString("en-IN",{
+                                    hour:"2-digit",
+                                    minute:"2-digit",
+                                }) }
+                            </small>
+                        </div>
+                    ))
+                }
+                
             </div>
         </div>
     )
