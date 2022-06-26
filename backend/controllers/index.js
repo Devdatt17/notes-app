9ee1c3fb50e1645db7e674e7a80217a8d1a61652
Devdatt17 <devdattpandit99@gmail.com>
2022-06-26 14:16:41 +0530

    added id and last modified date

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require('uuid')
 let notes = []
 
 //Display all notes
@@ -7,8 +8,12 @@ const displayAllNotes = (req,res)=>{
 
 //Added new note
 const addNewNotes = (req,res)=>{
-    notes.push({...req.body})
-    res.send(`Note no. ${req.body.id} added to the database`)
+    notes.push({
+        id:uuidv4(),
+        ...req.body,
+        lastModified: Date.now()
+    })
+    res.send(`New note added to the database`)
 }
 
 //Display single note
@@ -23,13 +28,13 @@ const updateSingleNote = (req,res)=>{
     req.body.title ? updateNote.title = req.body.title : updateNote.title
     req.body.message ? updateNote.message = req.body.message : updateNote.message
 
-    res.send(`Note with the id ${updateNote.id} is updated`)
+    res.send(`Note with the id ${req.params.id} is updated`)
 }
 
 //Delete single note
 const deleteSingleNote = (req,res)=>{
-    notes = notes.filter((note)=> note.id !== req.body.id)
-    res.send(`Note with id ${req.body.id} is deleted`)
+    notes = notes.filter((note)=> note.id !== req.params.id)
+    res.send(`Note with id ${req.params.id} is deleted`)
 }
 module.exports = {
     displayAllNotes,
