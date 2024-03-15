ff826b460b25dd2b3d82b84598f29a17d99759f4
Devdatt17 <devdattpandit99@gmail.com>
2024-03-16 02:44:17 +0530

    implemented view endpoint to fetch and display data

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,3 +1,21 @@
+import axios from 'axios'
+
+export const VIEW_NOTE = () => async dispatch => {
+    try{
+        const res = await axios.get(`https://notes-app-backend-devdatt17s-projects.vercel.app/`)
+        dispatch( {
+            type: "VIEW_NOTE",
+            payload: res.data
+        })
+    }
+    catch(e){
+        dispatch( {
+            type: "ERROR_NOTE",
+            payload: console.log(e),
+        })
+    }
+}
+
 export const ADD_NOTE=  (payload)=>{
     return {
         type: 'ADD_NOTE',
